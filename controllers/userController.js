9b30b9c5dd267b49fda293d49ff424f26897da1b
Owin Ekanayake <owinlakshitha@gmail.com>
2024-12-23 22:38:44 +0530

    Add password hashing with bcrypt for user data security

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,13 @@
 import User from "../models/user.js";
 import jwt from "jsonwebtoken";
+import bctypt from "bcrypt";
 
 export function postUser(req, res) {
   const user = req.body;
+  const password = req.body.password;
+
+  const passwordHash = bctypt.hashSync(password, 10);
+  user.password = passwordHash;
 
   const newUser = new User(user);
   newUser
@@ -26,33 +31,54 @@ export function postUser(req, res) {
         });
       }
     });
-} 
+}
 
-export function loginUser(req,res){
+export function loginUser(req, res) {
   const credentials = req.body;
-  User.findOne({email : credentials.email, password : credentials.password}).then(
-    (user)=>{
+
+  User.findone({ email: credentials.email })
+    .then((user) => {
       if (user == null) {
         res.status(404).json({
-          message : "User Not Found"
-        })
-      }else{
-        const payload = {
-          id : user.id,
-          email : user.email,
-          firstName : user.firstName,
-          lastName : user.lastName,
-          type : user.type,
-        };
+          message: "User not found",
+        });
+      } else {
+        bctypt
+          .compare(credentials.password, user.password)
+          .then((isMatch) => {
+            if (isMatch) {
+              const paylord = {
+                id: user.id,
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                type: user.type,
+              };
+              const token = jwt.sign(paylord, process.env.JWT_KEY, {
+                expiresIn: "1h",
+              });
 
-        const token = jwt.sign(payload,process.env.JWT_KEY,{expiresIn : "1h"})
-
-        res.json({
-          message : "User Found",
-          user : user,
-          token : token
-        })
+              res.json({
+                message: "User found",
+                user: user,
+                token: token,
+              });
+            } else {
+              res.status(401).json({
+                message: "Invalid password",
+              });
+            }
+          })
+          .catch(() => {
+            res.status(500).json({
+              message: "Errpr during password comparison",
+            });
+          });
       }
-    }
-  )
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "Error during user lookup",
+      });
+    });
 }
