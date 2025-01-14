4fbac1b496b9344679086d32273c31b2adb448a6
Owin Ekanayake <owinlakshitha@gmail.com>
2025-01-14 23:02:50 +0530

    Fix authorization check in updateCategory function

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -94,7 +94,7 @@ export function getCategoryByName(req, res) {
 
 export function updateCategory(req, res) {
   
-  if (isAdminValid(req)) {
+  if (!isAdminValid(req)) {
     return res.status(404).json({
       message: "Unauthorized"
     })
