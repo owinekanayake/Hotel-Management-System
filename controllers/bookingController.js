9940aa664e6e6df32c4a9d2d479fda72ac536b2c
Owin Ekanayake <owinlakshitha@gmail.com>
2025-01-20 01:04:20 +0530

    Enhance booking creation and retrieval functions with improved error handling and additional category-based booking creation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -12,78 +12,103 @@ export function createBooking(req, res) {
   const startingId = 1200;
 
   Booking.countDocuments({})
-  .then((count) => {
-    console.log(count);
-    const newId = startingId + count + 1;
-    const newBooking = new Booking({
-      bookingId: newId,
-      roomId: req.body.roomId,
-      email: req.body.user.email,
-      start: req.body.start,
-      end: req.body.end,
-    });
-    newBooking
-      .save()
-      .then((result) => {
-        res.json({
-          message: "Booking created successfully",
-          result: result,
-        });
-      })
-      .catch((err) => {
-        res.json({
-          message: "Booking creation failed",
-        });
+    .then((count) => {
+      console.log(count);
+      const newId = startingId + count + 1;
+      const newBooking = new Booking({
+        bookingId: newId,
+        roomId: req.body.roomId,
+        email: req.body.user.email,
+        start: req.body.start,
+        end: req.body.end,
       });
-  })
-  .catch((err) => {
-    res.json({
-      message: "Booking creation failed",
-      error: err,
+      newBooking
+        .save()
+        .then((result) => {
+          res.json({
+            message: "Booking created successfully",
+            result: result,
+          });
+        })
+        .catch((err) => {
+          res.json({
+            message: "Booking creation failed",
+          });
+        });
+    })
+    .catch((err) => {
+      res.json({
+        message: "Booking creation failed",
+        error: err,
+      });
     });
-  });
 }
 
-export default function getAllBooking(req,res){
-  Booking.find().then(
-    (result)=>{
+export default function getAllBooking(req, res) {
+  Booking.find()
+    .then((result) => {
       res.json({
         message: "Booking fetched successfully",
-        result: result
-      })
-    }
-  ).catch(
-    (err)=>{
+        result: result,
+      });
+    })
+    .catch((err) => {
       res.json({
-        message : "failed to get all booking",
-        error: err
-      })
-    }
-  )
+        message: "failed to get all booking",
+        error: err,
+      });
+    });
 }
 
-export function retrieveBookingByDate(req,res){
-  const start = req.body.start
-  const end = req.body.end
+export function retrieveBookingByDate(req, res) {
+  const start = req.body.start;
+  const end = req.body.end;
 
   Booking.find({
     start: {
-      $gte : new Date(start)
+      $gte: new Date(start),
     },
-    end : {
-      $lt : new Date(end)
-    }
-  }).then(
-    (result)=>{
+    end: {
+      $lt: new Date(end),
+    },
+  })
+    .then((result) => {
       res.json({
         message: "Booking fetched successfully",
-        result: result
-      })
-    }
-  ).catch((err)=>{
+        result: result,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        message: "Failed to get booking",
+        error: err,
+      });
+    });
+}
+
+export function createBookingUsingCategory(req, res) {
+  const start = new Date(req.body.start);
+  const end = new Date(req.body.end);
+
+  Booking.find({
+    $or: [
+      {
+        start: {
+          $gte: start,
+          $lt: end,
+        },
+      },
+      {
+        end: {
+          $gt : start,
+          $lte : end
+        }
+      }
+    ],
+  }).then((res)=>{
     res.json({
-      message: "Failed to get booking",
-      error: err
+      message : "Booking created successfully",
+      result : res
     })
   })
 }
