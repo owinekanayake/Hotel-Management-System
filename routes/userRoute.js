7a03bc86a46e603b32fd5ac4876ee5bba6324a9c
Owin Ekanayake <owinlakshitha@gmail.com>
2025-01-21 23:07:53 +0530

    Add email route to userRouter for sending sample emails

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { getUser, loginUser, postUser } from '../controllers/userController.js';
+import { getUser, loginUser, postUser, sendSampleEmail } from '../controllers/userController.js';
 import { validateUserFields } from '../middleware/validateUserFields.js';
 const userRouter = express.Router()
 
 userRouter.post("/",validateUserFields,postUser)
 userRouter.post("/login",loginUser)
 userRouter.get("/",getUser)
-
+userRouter.post("/email",sendSampleEmail)
 export default userRouter;
 
