2113ee291cca72f15acfe7bebd75466f3411ebd2
Owin Ekanayake <owinlakshitha@gmail.com>
2025-01-23 20:34:09 +0530

    Replace getUser route with getAllUsers to enhance user retrieval functionality

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import { getUser, loginUser, postUser, verifyUserEmail} from '../controllers/userController.js';
+import { getAllUsers, loginUser, postUser, verifyUserEmail} from '../controllers/userController.js';
 import { validateUserFields } from '../middleware/validateUserFields.js';
 const userRouter = express.Router()
 
 userRouter.post("/",validateUserFields,postUser)
 userRouter.post("/login",loginUser)
-userRouter.get("/",getUser)
+userRouter.get("/",getAllUsers)
 userRouter.post("/verify-email", verifyUserEmail)
 export default userRouter;
